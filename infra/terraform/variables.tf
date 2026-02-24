395f31a641733025293ed738a790f5be7c1d24e6
Evan Perry <eperry2688@gmail.com>
2026-02-24 16:34:06 -0700

    Switch Ec2 to t2.micro on TF

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -19,7 +19,7 @@ variable "aws_region" {
 variable "instance_type" {
   description = "EC2 instance type for the application."
   type        = string
-  default     = "t3.micro"
+  default     = "t2.micro"
 }
 
 variable "app_port" {
